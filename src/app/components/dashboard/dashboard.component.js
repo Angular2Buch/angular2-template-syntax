39b7b0d2115372097695872d9d6c9ee58eef0030
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-10-28 23:29:29 +0100

    that should be the correct setting, if '@reactivex/rxjs' wouldn't have such an annoying name
    see: https://github.com/jspm/registry/pull/596
    see: https://github.com/jspm/jspm.io/issues/26#issuecomment-152013360
    
    conclusion: right now this can't be loaded from https://npm.jspm.io/ - as far as I know

--- src/app/components/dashboard/dashboard.component.js
+++ src/app/components/dashboard/dashboard.component.js
@@ -8,14 +8,14 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require('angular2/angular2');
-var car_component_ts_1 = require('../car/car.component.ts');
-var car_ts_1 = require('../../models/car.ts');
+var car_component_1 = require('../car/car.component');
+var car_1 = require('../../models/car');
 var DashboardComponent = (function () {
     function DashboardComponent() {
         this.totalDamages = 0;
         this.cars = [
-            new car_ts_1.default('ng-car 1.0'),
-            new car_ts_1.default('ng-car 2.0'),
+            new car_1.default('ng-car 1.0'),
+            new car_1.default('ng-car 2.0'),
             null,
             undefined
         ];
@@ -26,7 +26,7 @@ var DashboardComponent = (function () {
     DashboardComponent = __decorate([
         angular2_1.Component({ selector: 'dashboard' }),
         angular2_1.View({
-            directives: [car_component_ts_1.default, angular2_1.NgFor, angular2_1.NgIf],
+            directives: [car_component_1.default, angular2_1.NgFor, angular2_1.NgIf],
             template: "\n    <div class=\"row\">\n      <template [ng-if]=\"totalDamages > 0\">\n        <div class=\"col-md-4\">\n          <p class=\"lead\">Reported Damages <span class=\"badge\">{{ totalDamages }}</span></p>\n        </div>\n      </template>\n    </div>\n    <div\n      *ng-for=\"#c of cars\"\n      class=\"row\">\n      <div class=\"col-md-4\">\n        <car [model]=\"c\" (damaged)=\"notifyCarDamaged($event)\" var-car></car>\n      </div>\n      <div class=\"col-md-3\">\n        <button\n          (click)=\"car.getTankCapacity()\"\n          [disabled]=\"c == null\"\n          class=\"btn btn-primary\">\n          Get tank capacity\n        </button>\n      </div>\n    </div>\n  "
         }), 
         __metadata('design:paramtypes', [])
